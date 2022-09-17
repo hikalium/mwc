9321f5d150156453b50c64da258a71dffa7b2f12
hikalium <hikalium@hikalium.com>
2022-09-17 10:25:15 +0900

    save

--- dist/web/extension.js
+++ dist/web/extension.js
@@ -1,7 +1,72 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ([
-/* 0 */,
+/* 0 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.deactivate = exports.activate = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __webpack_require__(1);
+function listAllFiles(fs, dir) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(`dir: ${dir}`);
+        let files = [];
+        let entries = yield vscode.workspace.fs.readDirectory(dir);
+        for (const e of entries) {
+            console.log(`e: ${e}}`);
+            if (e[1] == vscode.FileType.File) {
+                files.push(e[0]);
+            }
+            if (e[1] == vscode.FileType.Directory) {
+                console.log(`${e[0]}`);
+                files = files.concat(yield listAllFiles(fs, vscode.Uri.joinPath(dir, e[0])));
+            }
+        }
+        return files;
+    });
+}
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "helloworld-web-sample" is now active in the web extension host!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('helloworld-web-sample.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        if (!vscode.workspace.workspaceFolders || !vscode.workspace.workspaceFolders[0]) {
+            vscode.window.showErrorMessage("No workspace found");
+            return;
+        }
+        let wf = vscode.workspace.workspaceFolders[0].uri;
+        listAllFiles(vscode.workspace.fs, wf).then(files => {
+            // Display a message box to the user
+            vscode.window.showInformationMessage(`Hello ! ${files.length}`);
+        });
+    });
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+
+
+/***/ }),
 /* 1 */
 /***/ ((module) => {
 
@@ -28,49 +93,22 @@ module.exports = require("vscode");
 /******/ 		};
 /******/ 	
 /******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
-var exports = __webpack_exports__;
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = __webpack_require__(1);
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld-web-sample" is now active in the web extension host!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('helloworld-web-sample.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from helloworld-web-sample in a web extension host!');
-    });
-    context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-// this method is called when your extension is deactivated
-function deactivate() { }
-exports.deactivate = deactivate;
-
-})();
-
-var __webpack_export_target__ = exports;
-for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
-if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(0);
+/******/ 	var __webpack_export_target__ = exports;
+/******/ 	for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+/******/ 	if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=extension.js.map
--- src/web/extension.ts
+++ src/web/extension.ts
@@ -2,6 +2,23 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+async function listAllFiles(fs: vscode.FileSystem, dir: vscode.Uri): Promise<Array<string>> {
+	console.log(`dir: ${dir}`)
+	let files: Array<string> = [];
+	let entries = await vscode.workspace.fs.readDirectory(dir);
+	for(const e of entries) {
+		console.log(`e: ${e}}`)
+		if (e[1] == vscode.FileType.File) {
+			files.push(e[0]);
+		}
+		if (e[1] == vscode.FileType.Directory) {
+			console.log(`${e[0]}`);
+			files = files.concat(await listAllFiles(fs, vscode.Uri.joinPath(dir, e[0])));
+		}
+	}
+	return files;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -15,13 +32,21 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('helloworld-web-sample.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
+		if (!vscode.workspace.workspaceFolders || !vscode.workspace.workspaceFolders[0]) {
+			vscode.window.showErrorMessage("No workspace found");
+			return;
+		}
+		let wf = vscode.workspace.workspaceFolders[0].uri;
+		listAllFiles(vscode.workspace.fs, wf).then(files => {
+			// Display a message box to the user
+			vscode.window.showInformationMessage(`Hello ! ${files.length}`);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from helloworld-web-sample in a web extension host!');
+		});
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
